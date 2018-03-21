461065526c0950340d47b35546798c379a3078f0
Peter Watkins <Peter.Watkins@csiro.au>
2018-03-21 12:52:37 +1100

    using plots based on spread data

--- scripts/plots_tidy.R
+++ scripts/plots_tidy.R
@@ -4,6 +4,16 @@ library(ggplot2) # assumes that 'tidyverse' library is present
 
 ## object as 'tidy'
 
-tmp <- bcfa_m %>%
+MOA <- bcfa_m %>%
   filter(FA == "MOA")
 
+ggplot(MOA, aes(x = as.factor(Year), y = content)) +
+  geom_point()
+
+bcfa_m  %>%  
+ggplot(filter(bcfa_m, FA == "MNA"), aes(x = as.factor(Year), y = content)) +
+  geom_point()
+
+ggplot(bcfa_spread, aes(x=`MOA`, y=`C17.0`, color=as.factor('Year'))) +
+  geom_point() +
+  scale_x_log10()
