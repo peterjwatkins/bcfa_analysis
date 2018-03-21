714fc697813952943af432027fdc2e248eae81a0
Peter Watkins <Peter.Watkins@csiro.au>
2018-03-21 12:51:44 +1100

    included spread statement

--- scripts/data_wrangling.R
+++ scripts/data_wrangling.R
@@ -4,10 +4,12 @@ bcfa <- read_csv("data/bcfa_C17_fa.csv")
 ## Start fresh and assume no prior
 #--- Create 'tidy' frame
 bcfa_tidy <-
-  bcfa %>%
-  gather(key = FA, val = content, -Year) %>%
-  mutate(temp_id = 1:n())                     # add seq of integers as unique ID
+  bcfa %>% 
+  mutate(temp_id = 1:n()) %>% 
+  gather(key = FA, val = content , -Year, -temp_id)
+
 
 #  Now, we spread data in FA
 bcfa_spread <- bcfa_tidy %>% 
-  spread(FA, content)
+#  select(-temp_id) %>% 
+  spread(FA, content)#, -temp_id, -Year) #, -Year, -temp_id)
